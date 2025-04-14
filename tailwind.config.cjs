256891489f10b7bd8821513c35dfb93a76ec254d
Arsen Grigoryan <arsen.grigoryan555@gmail.com>
2025-04-14 22:33:56 +0400

    Added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
         "./public/**/*.html"
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Manrope', 'sans-serif'],
+            },
+        },
     },
     plugins: [require('@tailwindcss/typography')],
 }
