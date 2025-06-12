57b8b279027545f8ca0c692cc1f2c0da6b3d8b8d
Arsen Grigoryan <arsen.grigoryan555@gmail.com>
2025-06-12 23:10:09 +0400

    Made Service dropdown both for desktop and mobile

---
+++ src/components/reactComponents/ServiceDropdown.tsx
@@ -0,0 +1,19 @@
+import { Dropdown } from "antd";
+
+const items = [
+    { key: "1", label: <a href="/services/internal_audit/">Internal Audit</a> },
+    { key: "2", label: <a href="/services/it_audit/">IT Audit</a> },
+    { key: "3", label: <a href="/services/eqa_audit/">EQA</a> },
+    { key: "4", label: <a href="/services/it_support/">IT Support</a> },
+    { key: "4", label: <a href="/services/financial_management/">Financial Management</a> },
+];
+
+export default function ServiceDropdown() {
+    return (
+        <Dropdown placement={"bottom"} menu={{ items }} trigger={["hover"]} getPopupContainer={(triggerNode) => triggerNode.parentElement!}>
+            <a className="cursor-pointer flex items-center gap-1 hover:text-[#003B71] transition">
+                Services
+            </a>
+        </Dropdown>
+    );
+}
